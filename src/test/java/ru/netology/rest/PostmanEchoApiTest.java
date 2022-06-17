8016e435c58106b24c71dd547ce559e2598d722f
Golubev <golubev.s.g.25@gmail.com>
2022-06-17 09:40:49 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoApiTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoApiTest {
+    @Test
+    void shouldPostData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+
+    }
+}
